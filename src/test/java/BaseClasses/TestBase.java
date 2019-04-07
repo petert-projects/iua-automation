d9c52e7d4f2af1cafccbef6c318abed5188ab2ef
Peter Tarasenko <petert@wix.com>
2019-04-07 17:57:40 +0300

    run tests on selenium grid

--- src/test/java/BaseClasses/TestBase.java
+++ src/test/java/BaseClasses/TestBase.java
@@ -3,7 +3,11 @@ package BaseClasses;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Created by Petert on 4/5/19.
@@ -12,8 +16,9 @@ public class TestBase {
     protected static WebDriver driver;
 
     @Before
-    public void beforeTest() {
-        driver = new ChromeDriver();
+    public void beforeTest() throws MalformedURLException {
+        ChromeOptions chromeOptions =  new ChromeOptions();
+        driver = new RemoteWebDriver(new URL("http://5bf6b9ad.ngrok.io/wd/hub"),chromeOptions);
     }
 
     @After
